eafa861f485dcc56be7dc04d7759a7eac1d70f9b
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-02 23:14:59 -0300

    appy css on component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { styled } from '@/styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rockeseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
+
 export default function Home() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+  return <Button>Hello world</Button>
 }
