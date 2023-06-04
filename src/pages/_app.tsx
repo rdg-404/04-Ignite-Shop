53611cc2f0450f9052cab4c2c37d90848d97cab8
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-03 23:06:17 -0300

    otimization of image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import LogoImg from '../assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={LogoImg.src} alt="Ignite Logo" />
+        <Image src={LogoImg} alt="Ignite Logo" />
       </Header>
 
       <Component {...pageProps} />
