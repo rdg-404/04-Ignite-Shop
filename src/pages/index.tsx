820938f5f2d23aeef61e1ff6fa95b0537fd89b1a
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-05 20:38:26 -0300

    waiting node datas load for show the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { HomeContainer, Product } from '@/styles/pages/home'
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -17,6 +17,7 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={camiseta01} width={520} height={480} alt=""></Image>
 
@@ -54,4 +55,17 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+// espera as informações do lado do servdidor carregar para mostrar a pagina
+export const getServerSideProps = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  }
 }
