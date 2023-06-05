0c12e0a843d2d29b9f88197cf6bf38181e5dfa7f
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-04 22:38:59 -0300

    add image for page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,37 @@
-import { styled } from '@/styles'
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import Image from 'next/image'
+import camiseta01 from '../assets/camisetas/1.png'
+import camiseta02 from '../assets/camisetas/2.png'
+import camiseta03 from '../assets/camisetas/3.png'
 
 export default function Home() {
-  return <Button>Hello world</Button>
+  return (
+    <div>
+      <div>
+        <Image src={camiseta01} width={520} height={480} alt=""></Image>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </div>
+
+      <div>
+        <Image src={camiseta02} width={520} height={480} alt=""></Image>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </div>
+
+      <div>
+        <Image src={camiseta03} width={520} height={480} alt=""></Image>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </div>
+    </div>
+  )
 }
