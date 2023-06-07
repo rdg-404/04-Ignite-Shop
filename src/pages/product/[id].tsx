c10d76869fe48257b59597ff00ccd7cd0d01ecf5
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-07 20:27:34 -0300

    :bricks: get product datas from stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,6 +3,9 @@ import {
   ProductContainer,
   ProductDetails,
 } from '@/styles/pages/product'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 // import { useRouter } from 'next/router'
 
 export default function Product() {
@@ -26,4 +29,32 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['deafult_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
