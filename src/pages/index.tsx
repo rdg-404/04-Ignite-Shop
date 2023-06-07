864818d1c95e4abe801e16adf0cace1365ba2bb4
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-07 19:11:29 -0300

    :zap: Change ancor for link from nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { GetStaticProps } from 'next'
 
 import { useKeenSlider } from 'keen-slider/react'
@@ -30,23 +31,21 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product
-            href={`/product/${product.id}`}
-            key={product.id}
-            className="keen-slider__slide"
-          >
-            <Image
-              src={product.imageUrl}
-              width={520}
-              height={480}
-              alt=""
-            ></Image>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image
+                src={product.imageUrl}
+                width={520}
+                height={480}
+                alt=""
+              ></Image>
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
