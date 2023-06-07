af20836bf34cb4674ea0bbc09925992a81caa734
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-07 19:35:58 -0300

    :construction: Create html product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
-import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
+// import { useRouter } from 'next/router'
 
 export default function Product() {
-  const { query } = useRouter()
-  return <h1>Product{JSON.stringify(query)}</h1>
+  // const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae modi,
+          suscipit quia repudiandae iste architecto reprehenderit debitis,
+          itaque sapiente officiis, sequi eius quis doloremque quae delectus
+          assumenda molestiae possimus impedit!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
