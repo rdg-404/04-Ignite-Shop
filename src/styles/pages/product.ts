1b380009db6c5900a63d010d15159b5d0f7e2943
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-07 19:48:04 -0300

    :lipstick: style image container and fix maxwidth

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -6,10 +6,25 @@ export const ProductContainer = styled('main', {
   alignItems: 'stretch',
   gap: '4rem',
 
-  maxWidth: '1180',
+  maxWidth: 1180,
   margin: '0 auto ',
 })
 
-export const ImageContainer = styled('div', {})
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 656,
+  background: 'linear-gradient(0deg, #259CA9, #149D69)',
+  borderRadius: 8,
+  padding: '0.25rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+})
 
 export const ProductDetails = styled('div', {})
