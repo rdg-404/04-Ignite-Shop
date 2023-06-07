26677b1338c03d88ea21e94543dc7c55141a8e30
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-07 19:54:29 -0300

    :lipstick: style product details

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -27,4 +27,42 @@ export const ImageContainer = styled('div', {
   },
 })
 
-export const ProductDetails = styled('div', {})
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300',
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300',
+  },
+
+  button: {
+    marginTop: 'auto',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    },
+  },
+})
