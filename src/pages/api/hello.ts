4b9a35c3db94b03d64571ea4cdb0a0f2c55a3b33
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-09 13:40:43 -0300

    :sparkles: create checkout page with stripe

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.json({ message: 'hello world!' })
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const priceId = 'price_1NFm8dBFfemrteaRfWUcxp2u'
+
+  const successUrl = `${process.env.NEXT_URL}/success`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url, // redirect from stripe page
+  })
 }
