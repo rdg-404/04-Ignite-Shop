82229e7a013e133c4e4d99c92e0cf7d36ca04220
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-10 14:25:10 -0300

    :link: get info product from stripe

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+import { GetServerSideProps } from 'next'
+import { stripe } from '../lib/stripe'
+import Stripe from 'stripe'
 
 export default function Success() {
   return (
@@ -15,4 +18,27 @@ export default function Success() {
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  // console.log(session.line_items.data[0])
+
+  const custumerName = session.customer_details.name
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  return {
+    props: {
+      custumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
