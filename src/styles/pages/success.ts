c1120e85fb9654481f41347155132f51f031f205
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-10 13:33:06 -0300

    :lipstick: style main container

---
+++ src/styles/pages/success.ts
@@ -0,0 +1,38 @@
+import { styled } from '..'
+
+export const SuccessContainer = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: '0 auto',
+  height: 656,
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray100',
+  },
+
+  p: {
+    fontSize: '$xl',
+    color: '$gray300',
+    maxWidth: 560,
+    textAlign: 'center',
+    marginTop: '2rem',
+    lineHeight: 1.4,
+  },
+
+  a: {
+    display: 'block',
+    marginTop: '5rem',
+    fontSize: '$lg',
+    color: '$green500',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+
+    '&:hover': {
+      color: '$green300',
+    },
+  },
+})
+export const ImageContainer = styled('div', {})
